47f2cdc93f86647b18845dad1ffa42422f5c6c2c
Lizzie Mountain <em966@cam.ac.uk>
2025-04-22 15:34:51 +0100

    high pressure strat

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -13,7 +13,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         gamelib.debug_write('Random seed: {}'.format(seed))
 
     def on_game_start(self, config):
-        gamelib.debug_write('Configuring smart adaptive strategy...')
+        gamelib.debug_write('Setting up PressureWave Strategy...')
         self.config = config
         global WALL, SUPPORT, TURRET, SCOUT, DEMOLISHER, INTERCEPTOR, MP, SP
         WALL = config["unitInformation"][0]["shorthand"]
@@ -30,38 +30,42 @@ class AlgoStrategy(gamelib.AlgoCore):
         game_state = gamelib.GameState(self.config, turn_state)
         game_state.suppress_warnings(False)
 
-        self.build_defenses(game_state)
-        self.reactive_defense(game_state)
-
-        enemy_scouts = self.detect_enemy_mobile_units(game_state, SCOUT)
-        enemy_demolishers = self.detect_enemy_mobile_units(game_state, DEMOLISHER)
-        enemy_turrets = self.detect_enemy_unit(game_state, TURRET)
-
-        # Emergency defense
-        if enemy_scouts > 4 or enemy_demolishers > 2:
-            self.emergency_defense(game_state)
-
-        # Deploy interceptors early on
-        if game_state.turn_number % 2 == 0 and game_state.get_resource(MP) >= 2:
-            self.spawn_interceptors(game_state)
-
-        # Attack logic
-        if game_state.get_resource(MP) >= 8:
-            if enemy_turrets <= 4:
-                self.scout_rush(game_state)
-            else:
-                self.demolisher_push(game_state)
-
-        # Build extra supports if SP available
-        if game_state.get_resource(SP) >= 8:
-            self.build_extra_supports(game_state)
+        self.execute_pressure_strategy(game_state)
 
         game_state.submit_turn()
 
+    def execute_pressure_strategy(self, game_state):
+        self.build_defenses(game_state)
+        self.reactive_defense(game_state)
+
+        enemy_turrets = self.detect_enemy_unit(game_state, unit_type=TURRET)
+        enemy_scouts = self.detect_enemy_mobile_units(game_state, SCOUT)
+
+        # Cheese rush very early
+        if game_state.turn_number <= 1:
+            self.cheese_rush(game_state)
+            return
+
+        # Emergency defense if enemy Scout rushes
+        if enemy_scouts >= 5:
+            self.emergency_defense(game_state)
+
+        # Attack logic
+        if enemy_turrets <= 3 and game_state.get_resource(MP) >= 6:
+            self.scout_rush(game_state)
+        elif game_state.get_resource(MP) >= 10:
+            self.demolisher_push(game_state)
+
+        # Build extra Supports for late-game scaling
+        if game_state.get_resource(SP) >= 12:
+            self.build_extra_supports(game_state)
+
     def build_defenses(self, game_state):
-        # Basic strong defense layout
-        walls = [[13,13],[14,13],[12,13],[15,13],[11,12],[16,12],[10,11],[17,11]]
-        turrets = [[3,12],[24,12],[5,11],[22,11],[13,11],[14,11]]
+        walls = [
+            [13,13],[14,13],[12,13],[15,13],[11,13],[16,13],
+            [13,12],[14,12],[12,12],[15,12]
+        ]
+        turrets = [[3,12],[24,12],[13,11],[14,11]]
 
         for location in walls:
             game_state.attempt_spawn(WALL, location)
@@ -72,32 +76,10 @@ class AlgoStrategy(gamelib.AlgoCore):
             game_state.attempt_upgrade(location)
 
     def build_extra_supports(self, game_state):
-        # Build a strong economy
-        support_locations = [[13,3],[14,3],[13,4],[14,4],[12,4],[15,4]]
+        support_locations = [[13,3],[14,3],[13,4],[14,4],[12,3],[15,3]]
         game_state.attempt_spawn(SUPPORT, support_locations)
         for loc in support_locations:
             game_state.attempt_upgrade(loc)
-
-    def spawn_interceptors(self, game_state):
-        if game_state.get_resource(MP) >= 2:
-            game_state.attempt_spawn(INTERCEPTOR, [13,0], 2)
-
-    def scout_rush(self, game_state):
-        spawn_locations = [[13,0],[14,0]]
-        best_location = self.least_damage_spawn_location(game_state, spawn_locations)
-        game_state.attempt_spawn(SCOUT, best_location, 1000)
-
-    def demolisher_push(self, game_state):
-        spawn_location = [13,0]
-        game_state.attempt_spawn(DEMOLISHER, spawn_location, 5)
-
-    def emergency_defense(self, game_state):
-        emergency_turrets = [[12,11],[15,11],[11,11],[16,11]]
-        for loc in emergency_turrets:
-            game_state.attempt_spawn(TURRET, loc)
-            game_state.attempt_upgrade(loc)
-        if game_state.get_resource(MP) >= 3:
-            game_state.attempt_spawn(INTERCEPTOR, [13,0], 2)
 
     def reactive_defense(self, game_state):
         for location in self.scored_on_locations:
@@ -105,6 +87,28 @@ class AlgoStrategy(gamelib.AlgoCore):
             game_state.attempt_spawn(TURRET, repair_location)
             game_state.attempt_spawn(WALL, repair_location)
             game_state.attempt_upgrade(repair_location)
+
+    def cheese_rush(self, game_state):
+        spawn_locations = [[13,0],[14,0]]
+        best_location = self.least_damage_spawn_location(game_state, spawn_locations)
+        game_state.attempt_spawn(SCOUT, best_location, 1000)
+
+    def scout_rush(self, game_state):
+        spawn_options = [[13,0],[14,0]]
+        best_location = self.least_damage_spawn_location(game_state, spawn_options)
+        game_state.attempt_spawn(SCOUT, best_location, 1000)
+
+    def demolisher_push(self, game_state):
+        # Spawn Demolishers protected by some Interceptors
+        game_state.attempt_spawn(INTERCEPTOR, [13,0], 2)
+        game_state.attempt_spawn(DEMOLISHER, [13,0], 1000)
+
+    def emergency_defense(self, game_state):
+        # Quick walls and turrets when under heavy Scout attack
+        emergency_locations = [[12,11],[15,11],[13,11],[14,11]]
+        for loc in emergency_locations:
+            game_state.attempt_spawn(TURRET, loc)
+            game_state.attempt_upgrade(loc)
 
     def least_damage_spawn_location(self, game_state, location_options):
         damages = []
